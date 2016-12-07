40c6b311f494b6e6de5551d5d3a29196e01afe09
kaktuxis <miroslaw.pats@gmail.com>
2016-12-07 18:48:05 +0300

    install grunt-nanocss

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,13 +4,25 @@ module.exports = function(grunt) {
 		less: {
 			'dev': {
 				files: {
-					'syle.compiled.css': 'less/style.less'
+					'style.compiled.css': 'less/style.less'
+				}
+			}
+		}
+		cssnano: {
+			options: {
+				sourcemap: true
+			}
+			dist: {
+				files: {
+					'style.min.css': 'style.compiled.css'
 				}
 			}
 		}
 	});
-	// Load the plugin that provides the "less" task.
+	  	// Load the plugin that provides the "less" task.
 	grunt.loadNpmTasks("grunt-contrib-less");
+	grunt.loadNpmTasks("grunt-cssnano");
 	// Default task(s).
 	grunt.registerTask('default', ['less:dev']);
+	grunt.registerTask('develop', ['cssnano']);
 };
